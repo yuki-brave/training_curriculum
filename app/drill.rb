f939fd20b3ab915df760bbd35b5662869e2fe35a
yuki-brave <93358518+yuki-brave@users.noreply.github.com>
2022-03-01 12:08:50 +0900

    if → unlessへの書き換え

--- app/drill.rb
+++ app/drill.rb
@@ -1,8 +1,7 @@
-require "date"
-weeks = ["日曜日", "月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日"]
-day = Date.today.wday
-if day == 5
-  puts "今日は金曜日だ！！！"
-else
-  puts "今日は#{weeks[day]}"
+if a + b > 0
+  puts "計算結果は0より大きいです"
+end
+
+unless a + b <= 0
+  puts "計算結果は0より大きいです"
 end
