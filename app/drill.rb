ed0dcbf643522a1b7c3056a3a02fc7ee4509f139
yuki-brave <93358518+yuki-brave@users.noreply.github.com>
2022-03-03 19:34:07 +0900

    配列を利用したrubyプログラムの作成

--- app/drill.rb
+++ app/drill.rb
@@ -1,5 +1,12 @@
-sum = 0
-10.times do |i|
-  sum += i + 1
+def search(target_num, input)
+  input.each_with_index do |num, i|
+    if num == target_num
+      puts "#{i + 1}番目です"
+      return
+    end
+  end
+  puts "その数は含まれていません"
 end
-puts sum
+
+input = [3, 5, 9 ,12, 15, 21, 29, 35, 42, 51, 62, 78, 81, 87, 92, 93]
+search(11, input)
