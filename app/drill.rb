2a0efa9852d7dec84d0b506030275977aa1ca81e
yuki-brave <93358518+yuki-brave@users.noreply.github.com>
2022-03-05 09:54:01 +0900

    eachの入れ子

--- app/drill.rb
+++ app/drill.rb
@@ -1,13 +1,9 @@
-def check_name(str) 
-  if  str.include?(".")
-    puts "!エラー!記号は登録できません"
-  elsif str.include?(" ")
-    puts "!エラー!空白は登録できません"
-  else
-    puts "登録が完了しました"
-  end
-end
+fruits_price = [["apple", [200, 250, 220]], ["orange", [100, 120, 80]], ["melon", [1200, 1500]]]
 
-puts "登録したい名前を入力してください"
-str = gets
-check_name(str) 
+fruits_price.each do |fruits|
+  sum = 0
+  fruits[1].each do |i|
+    sum += i 
+  end
+  puts "#{fruits[0]}の合計見学は#{sum}円です"
+end
