4c2d9dc21a246f3ea275e95fef43f10728c6bc51
yuki-brave <93358518+yuki-brave@users.noreply.github.com>
2022-03-06 18:40:40 +0900

    if,else問題

--- app/drill.rb
+++ app/drill.rb
@@ -1,9 +1,12 @@
-fruits_price = [["apple", [200, 250, 220]], ["orange", [100, 120, 80]], ["melon", [1200, 1500]]]
-
-fruits_price.each do |fruits|
-  sum = 0
-  fruits[1].each do |i|
-    sum += i 
-  end
-  puts "#{fruits[0]}の合計見学は#{sum}円です"
+def in1to10(num, outside_mode)
+  if (num >= 1 && num <= 10) || outside_mode
+    puts "True"
+  else
+    puts "False"
+  end 
 end
+
+# 呼び出し例
+in1to10(5,false)
+in1to10(11,false) 
+in1to10(11,true) 
