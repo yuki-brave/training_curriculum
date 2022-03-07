a6710d66852e4a3fbfaaf7cda530b933cb4075b4
yuki-brave <93358518+yuki-brave@users.noreply.github.com>
2022-03-07 18:46:30 +0900

    インスタンスの生成

--- app/drill.rb
+++ app/drill.rb
@@ -1,12 +1,27 @@
-def in1to10(num, outside_mode)
-  if (num >= 1 && num <= 10) || outside_mode
-    puts "True"
-  else
-    puts "False"
-  end 
-end
-
-# 呼び出し例
-in1to10(5,false)
-in1to10(11,false) 
-in1to10(11,true) 
+class Fruit
+  def self.fresh
+    puts "採れたて新鮮な果物です"
+  end
+ 
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
+ 
+  def introduce
+    puts "#{@name}は#{@price}円です"
+  end
+ end
+ 
+ 
+ # 3つのインスタンスを生成してください
+ fruit1 = Fruit.new("リンゴ", 120)
+ fruit2 = Fruit.new("オレンジ", 200)
+ fruit3 = Fruit.new("イチゴ", 60)
+ 
+ # クラスメソッドを呼び出し、「採れたて新鮮な果実です」と表示してください
+ # インスタンス毎にインスタンスメソッドを呼び出し、「【名前】は【価格】円です」と表示してください
+ Fruit.fresh
+ fruit1.introduce
+ fruit2.introduce
+ fruit3.introduce
