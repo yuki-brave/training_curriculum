b181d47862e2e2c183f8a87038c3a60d3ea6fcc2
yuki-brave <93358518+yuki-brave@users.noreply.github.com>
2022-03-08 16:27:07 +0900

    任意の文字列を部分的に削除するプログラムの実装

--- app/drill.rb
+++ app/drill.rb
@@ -1,27 +1,7 @@
-class Fruit
-  def self.fresh
-    puts "採れたて新鮮な果物です"
-  end
- 
-  def initialize(name, price)
-    @name = name
-    @price = price
-  end
- 
-  def introduce
-    puts "#{@name}は#{@price}円です"
-  end
- end
- 
- 
- # 3つのインスタンスを生成してください
- fruit1 = Fruit.new("リンゴ", 120)
- fruit2 = Fruit.new("オレンジ", 200)
- fruit3 = Fruit.new("イチゴ", 60)
- 
- # クラスメソッドを呼び出し、「採れたて新鮮な果実です」と表示してください
- # インスタンス毎にインスタンスメソッドを呼び出し、「【名前】は【価格】円です」と表示してください
- Fruit.fresh
- fruit1.introduce
- fruit2.introduce
- fruit3.introduce
+def missing_char(str, n)
+  str.slice!(n - 1)
+  puts str
+end
+
+# 呼び出し例（1つ目の引数には対象となる文字列を、2つ目の引数には対象となる文字列から何番目の文字を削除するのかを指定しましょう）
+missing_char('kitten', 1)
