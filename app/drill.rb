6a432058b5d6c91275b17555d9983386202f4dca
yuki-brave <93358518+yuki-brave@users.noreply.github.com>
2022-03-20 10:15:52 +0900

    2つの文字列の末尾に対して一致・不一致を判断するプログラムの実装

--- app/drill.rb
+++ app/drill.rb
@@ -1,17 +1,16 @@
-def fizz_buzz
-  i =  1 
-  while i <= 100
-    if i % 15 == 0
-      puts "FizzBuzz"
-    elsif i % 5 == 0
-      puts "Buzz"
-    elsif i % 3 == 0
-      puts "Fuss"
-    else
-      puts i
-    end
-    i += 1
+def end_other(a, b)
+  a_down = a.downcase
+  b_down = b.downcase
+  a_len = a_down.length
+  b_len = b_down.length
+  a_sli = a_down.slice(-(b_len)..-1)
+  b_sli = b_down.slice(-(a_len)..-1)
+  if a_sli == b_down || b_sli == a_down
+    puts "True"
+  else
+    puts "False"
   end
 end
 
-fizz_buzz
+# 呼び出し例
+end_other('Hiabc', 'abc')
