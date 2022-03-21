7cd70d03eb941f44fe9780e703f91ec82f829a54
yuki-brave <93358518+yuki-brave@users.noreply.github.com>
2022-03-21 09:28:28 +0900

    if,else問題

--- app/drill.rb
+++ app/drill.rb
@@ -1,16 +1,8 @@
-def end_other(a, b)
-  a_down = a.downcase
-  b_down = b.downcase
-  a_len = a_down.length
-  b_len = b_down.length
-  a_sli = a_down.slice(-(b_len)..-1)
-  b_sli = b_down.slice(-(a_len)..-1)
-  if a_sli == b_down || b_sli == a_down
+def near_ten(num)
+  calc = num % 10
+  if calc <= 2 || calc >= 8
     puts "True"
   else
     puts "False"
   end
 end
-
-# 呼び出し例
-end_other('Hiabc', 'abc')
