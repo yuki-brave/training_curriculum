54bad69fa077676915341bb7ac6e1ed020a376ae
yuki-brave <93358518+yuki-brave@users.noreply.github.com>
2022-03-22 17:28:18 +0900

    預金システムのアルゴリズム問題

--- app/drill.rb
+++ app/drill.rb
@@ -1,8 +1,14 @@
-def near_ten(num)
-  calc = num % 10
-  if calc <= 2 || calc >= 8
-    puts "True"
+def withdraw(balance, amount)
+  fee = 110
+  calc = balance - amount - fee
+  if  calc >= 0
+    puts "#{amount}円引き落としました。残高は#{calc}円です"
   else
-    puts "False"
+    puts "残高不足です"
   end
 end
+
+balance = 100000
+puts "いくら引き落としますか？（て終了110円かかります）"
+amount = gets.to_i
+withdraw(balance, amount)
